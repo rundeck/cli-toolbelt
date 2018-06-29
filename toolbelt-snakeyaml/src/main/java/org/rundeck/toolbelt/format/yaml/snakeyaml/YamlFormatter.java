6ea9866ad5b4392ebdc2c465eeb2af9c6227aeb5
Greg Schueler <greg.schueler@gmail.com>
2018-06-29 13:05:21 -0700

    Json and yaml formatters support data transform function

--- toolbelt-snakeyaml/src/main/java/org/rundeck/toolbelt/format/yaml/snakeyaml/YamlFormatter.java
+++ toolbelt-snakeyaml/src/main/java/org/rundeck/toolbelt/format/yaml/snakeyaml/YamlFormatter.java
@@ -1,67 +1,77 @@
 package org.rundeck.toolbelt.format.yaml.snakeyaml;
 
+import org.rundeck.toolbelt.BaseDataOutputFormatter;
 import org.rundeck.toolbelt.Formatable;
-import org.rundeck.toolbelt.OutputFormatter;
-import org.rundeck.toolbelt.ToStringFormatter;
 import org.rundeck.toolbelt.OutputFormatter;
 import org.rundeck.toolbelt.ToStringFormatter;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.representer.Representer;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
 /**
- * Format objects as YAML, this will convert any Map/Collection into Yaml,
- * and any Object that implements {@link Formatable} and returns a non-null Map or List.
- * If the object does not correspond to one of those inputs, the base formatter will be used
+ * Format objects as YAML, this will convert any Map/Collection into Yaml, and any Object that implements {@link
+ * Formatable} and returns a non-null Map or List. If the object does not correspond to one of those inputs, the base
+ * formatter will be used
  */
-public class YamlFormatter implements OutputFormatter {
+public class YamlFormatter extends BaseDataOutputFormatter {
     private Yaml yaml;
-    private OutputFormatter base;
+
 
     /**
      */
     public YamlFormatter() {
-        this.yaml = new Yaml();
-        this.base = new ToStringFormatter();
+        this(new ToStringFormatter());
     }
 
     /**
      */
     public YamlFormatter(Representer representer, DumperOptions options) {
+        super(new ToStringFormatter());
         this.yaml = new Yaml(representer, options);
-        this.base = new ToStringFormatter();
     }
 
-    /**
-     */
-    public YamlFormatter(DumperOptions options) {
-        this.yaml = new Yaml(options);
-        this.base = new ToStringFormatter();
+    public YamlFormatter(
+            final Function<Object, Optional<Formatable>> dataFormatter,
+            final Yaml yaml
+    ) {
+        super(dataFormatter);
+        this.yaml = yaml;
     }
+
 
     /**
      * @param base base formatter
      */
     public YamlFormatter(final OutputFormatter base) {
-        this();
-        this.base = base;
+        super(base);
+        this.yaml = new Yaml();
+    }
+
+    public YamlFormatter(
+            final OutputFormatter base,
+            final Function<Object, Optional<Formatable>> dataFormatter,
+            final Yaml yaml
+    ) {
+        super(base, dataFormatter);
+        this.yaml = yaml;
     }
 
     /**
      * @param base base formatter
      */
     private YamlFormatter(Yaml yaml, final OutputFormatter base) {
+        super(base);
         this.yaml = yaml;
-        this.base = base;
     }
 
     @Override
-    public OutputFormatter withBase(final OutputFormatter base) {
-        return new YamlFormatter(this.yaml, base);
+    protected OutputFormatter withBase(
+            final Function<Object, Optional<Formatable>> dataFormatter, final OutputFormatter base
+    ) {
+        return new YamlFormatter(base, dataFormatter, yaml);
     }
 
     /**
@@ -69,27 +79,17 @@ public class YamlFormatter implements OutputFormatter {
      * @param options yaml options
      */
     public YamlFormatter(final OutputFormatter base, DumperOptions options) {
-        this.base = base;
+        super(base);
         this.yaml = new Yaml(options);
     }
 
     @Override
-    public String format(final Object o) {
-        if (o instanceof Map) {
-            return yaml.dump(o);
-        } else if (o instanceof Collection) {
-            return yaml.dump(o);
-        } else if (o instanceof Formatable) {
-            Formatable o1 = (Formatable) o;
-            List<?> objects = o1.asList();
-            if (null != objects) {
-                return format(objects);
-            }
-            Map<?, ?> map = o1.asMap();
-            if (null != map) {
-                return format(map);
-            }
-        }
-        return base.format(o);
+    protected boolean canFormatObject(final Object value) {
+        return true;
+    }
+
+    @Override
+    protected String formatObject(final Object o) {
+        return yaml.dump(o);
     }
 }
